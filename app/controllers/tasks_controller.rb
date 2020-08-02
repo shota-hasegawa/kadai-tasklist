a20da3b82003646ce2c0938176cefc4c30a21948
shota.hasegawa <pphasesoccerv@gmail.com>
2020-08-02 12:49:57 +0000

    tasks_controller

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -3,9 +3,7 @@ class TasksController < ApplicationController
     before_action :correct_user,only: [:show, :edit, :update, :destroy]
     
     def index
-        if logged_in?
             @tasks = current_user.tasks.order(id: :desc).page(params[:page]).per(20)
-        end
     end
     
     def show
